b619d0c796d6c8d91f221d4ba526c2492d313811
pengpn <312924860@qq.com>
2018-03-19 10:08:09 +0800

    增加helper方法，并且自动加载

---
+++ app/helpers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 自定义辅助函数
+ * @author pnpeng
+ * @date 2018-03-16
+ */
+
+if (! function_exists('rules')) {
+    /*
+     * 取当前路由对应的表单验证规则
+     */
+    function rules()
+    {
+        list($class) = explode('@',request()->route()->getActionName());
+        $request_class = 'App\\Http\\Requests\\'.str_replace('Controller', 'Request', array_last(explode('\\', $class)));
+        $rules = (new $request_class)->rules();
+        return $rules;
+    }
+}
